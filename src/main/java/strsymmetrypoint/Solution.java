0b0e7f4eabe11c8c7cfbde64f1d156f2c4ebeec1
dneimanis <davis.neimanis@gmail.com>
2019-03-04 00:39:23 +0200

    StrSymmetryPoint - done Unit tests - done

---
+++ src/main/java/strsymmetrypoint/Solution.java
@@ -0,0 +1,30 @@
+package strsymmetrypoint;
+
+class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(solution("racecar"));
+
+    }
+
+    static int solution(String S) {
+
+        int symmetryIndex = S.length() / 2;
+
+        if (S.length() % 2 == 0)
+            return -1;
+        if (S.length() == 1)
+            return 0;
+
+        String firstHalf = S.substring(0, symmetryIndex);
+        String secondHalf = S.substring(symmetryIndex + 1);
+        String reverseSecondHalf = new StringBuffer(secondHalf).reverse().toString();
+
+        if (firstHalf.equals(reverseSecondHalf)) {
+            return symmetryIndex;
+        } else {
+            return -1;
+        }
+
+    }
+}
